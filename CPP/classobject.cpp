dd081f10d0985307cd833756daf6ac4afde0d94c
Abhijit Dhara <51172366+cybertron21@users.noreply.github.com>
2019-11-10 09:59:51 +0530

    Add files via upload

---
+++ CPP/classobject.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class myobjects
+{
+	public:
+		void printSomething()
+		{
+			cout << "Rinkia k papa hihi haste" << endl;
+		}
+
+};
+
+
+int main()
+{
+	myobjects mamamia;
+	mamamia.printSomething();
+	return 0;
+}
